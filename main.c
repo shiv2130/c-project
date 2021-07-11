4f7526264fc016c02c77bf7736de6e89a557925d
Shivanshu Prakash <shivanahuomili@gmail.com>
2021-07-11 20:24:43 +0530

    made a basic adding calculator

--- main.c
+++ main.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 
 int main(){
-    printf("Hello World! \n");
+    double num1, num2;
+    printf("Enter the first number ");
+    //%d is used for integer and %lf is used for doubles
+    scanf("%lf", &num1);
+    printf("Enter the second number ");
+    //%d is used for integer
+    scanf("%lf", &num2);
+    //and %f for doubles to when we have to pront a final answer
+    printf("Answer: %f", num1+num2);
+
     return 0;
 }
